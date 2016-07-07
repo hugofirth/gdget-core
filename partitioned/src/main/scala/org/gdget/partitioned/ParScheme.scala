2205bf5fb4eaedb089b1fcdd445d31e2e18129a4
Hugo Firth <me@hugofirth.com>
2016-07-08 00:11:35 +0100

    Closest to far to working with ParScheme, Strip back to V: ParV next

--- partitioned/src/main/scala/org/gdget/partitioned/ParScheme.scala
+++ partitioned/src/main/scala/org/gdget/partitioned/ParScheme.scala
@@ -1,20 +1,20 @@
 /** gdget
-  *
-  * Copyright (c) 2016 Hugo Firth
-  * Email: <me@hugofirth.com/>
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *
+ * Copyright (c) 2016 Hugo Firth
+ * Email: <me@hugofirth.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gdget.partitioned
 
 import cats.data.State
@@ -28,28 +28,36 @@ trait ParScheme[S[_]]{
   import ParScheme._
 
   //TODO: Use State here - but its a pain in the ass
-  def getPartition[G[_, _[_]]: Graph, E[_]: Edge, V](scheme: S[V], vertex: V, graph: G[V, E]): (S[V], PartitionId)
+  def getPartition[G[_, _[_]]: Graph, E[_]: Edge, V](scheme: S[V], vertex: V, graph: G[V, E]): (S[V], PartId)
 }
-
+      
 object ParScheme {
-
+      
   @inline def apply[S[_]: ParScheme]: ParScheme[S] = implicitly[ParScheme[S]]
-
-  /** Wrapper type for partition ids */
-  case class PartitionId(id: Int)
-
-  /** Default instance for Map[V, Int] */
-  implicit val mapScheme: ParScheme[Map[?, PartitionId]] = new ParScheme[Map[?, PartitionId]] {
-
-    /** Default partition index if a  */
-    private val defaultPartition = PartitionId(0)
-
-    override def getPartition[G[_, _[_]]: Graph, E[_]: Edge, V](scheme: Map[V, PartitionId], vertex: V, graph: G[V, E]): (Map[V, PartitionId], PartitionId) =
-      (scheme, scheme.getOrElse(vertex, defaultPartition))
+      
+  /** wrapper type for partition ids */
+  case class PartId(id: Int)
+      
+  implicit class PartIdOps(id: Int) {
+    def part = PartId(id)
   }
 
-  /** Default instance for (V) => Int */
-  implicit val fun1Scheme: ParScheme[? => PartitionId] = new ParScheme[? => PartitionId] {
-    override def getPartition[G[_, _[_]]: Graph, E[_]: Edge, V](scheme: (V) => PartitionId, vertex: V, graph: G[V, E]): ((V) => PartitionId, PartitionId) = (scheme, scheme(vertex))
+  /** default instance for Map[V, Int] */
+  implicit val mapInstance: ParScheme[Map[?, PartId]] = new ParScheme[Map[?, PartId]] {
+      
+    /** default partition index if a vertex cannot be found in the Map */
+    private val defaultPartition = 0.part
+      
+    override def getPartition[G[_, _[_]]: Graph, E[_]: Edge, V](scheme: Map[V, PartId],
+                                                                vertex: V, graph: G[V, E]): (Map[V, PartId], PartId) =
+      (scheme, scheme.getOrElse(vertex, defaultPartition))
+  }
+      
+  /** default instance for (V) => Int */
+  implicit val fun1sScheme: ParScheme[? => PartId] = new ParScheme[? => PartId] {
+    override def getPartition[G[_, _[_]]: Graph, E[_]: Edge, V](scheme: (V) => PartId,
+                                                                vertex: V,
+                                                                graph: G[V, E]): ((V) => PartId, PartId) =
+      (scheme, scheme(vertex))
   }
 }
